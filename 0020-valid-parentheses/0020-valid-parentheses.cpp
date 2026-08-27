5ac88b395231ba0bf49ee875276f3eea945a5216
harshilgothi12 <harshil.gothi146676@marwadiuniversity.ac.in>
2026-08-27 09:41:58 +0530

    Time: 0 ms (100%), Space: 8.9 MB (37.37%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+
+        for(char c : s) {
+
+            if(c == '(' || c == '[' || c == '{') {
+                st.push(c);
+            }
+            else {
+                if(st.empty())
+                    return false;
+
+                char top = st.top();
+
+                if(c == ')' && top != '(')
+                    return false;
+
+                if(c == ']' && top != '[')
+                    return false;
+
+                if(c == '}' && top != '{')
+                    return false;
+
+                st.pop();
+            }
+        }
+
+        return st.empty();
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
